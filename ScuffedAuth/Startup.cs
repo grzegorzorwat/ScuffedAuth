dcf4f1cbc6a41f20d46d346104efcde9e8642352
grzegorzorwat <grzegorz.orwat95@gmail.com>
2020-11-19 01:50:00 +0100

    chore: basic authentication for swagger
    In client credentails grant client id and secret are passed in basic
    authentication header.

--- ScuffedAuth/Startup.cs
+++ ScuffedAuth/Startup.cs
@@ -18,11 +18,33 @@ namespace ScuffedAuth
 
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ScuffedAuth", Version = "v1" });
+                c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "basic",
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "basic"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
             });
         }
 
