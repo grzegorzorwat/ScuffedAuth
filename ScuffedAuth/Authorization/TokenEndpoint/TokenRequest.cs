40f9d4f3ef31cb66ab8f5e3069480eca1c054a5d
grzegorzorwat <grzegorz.orwat95@gmail.com>
2020-11-19 02:59:39 +0100

    feat: require GrantType in TokenRequest

--- ScuffedAuth/Authorization/TokenEndpoint/TokenRequest.cs
+++ ScuffedAuth/Authorization/TokenEndpoint/TokenRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace ScuffedAuth.Authorization.TokenEndpoint
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScuffedAuth.Authorization.TokenEndpoint
 {
     public class TokenRequest
     {
+        [Required]
         public GrantTypes GrantType { get; set; }
     }
 }
