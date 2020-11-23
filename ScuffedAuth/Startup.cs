2e48d38e34b0e4e2fec36fc2eff7c46a8a55eb49
grzegorzorwat <grzegorz.orwat95@gmail.com>
2020-11-23 23:11:31 +0100

    refactor: Array.Empty instead of zero-length array

--- ScuffedAuth/Startup.cs
+++ ScuffedAuth/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace ScuffedAuth
 {
@@ -43,7 +44,7 @@ namespace ScuffedAuth
                                     Id = "basic"
                                 }
                             },
-                            new string[] {}
+                            Array.Empty<string>()
                         }
                     });
                 });
