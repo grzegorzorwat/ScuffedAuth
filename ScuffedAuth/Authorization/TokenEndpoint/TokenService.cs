1f5570237d4de489937b3eaa30efcda3d147fcf7
grzegorzorwat <grzegorz.orwat95@gmail.com>
2020-11-25 00:22:23 +0100

    refactor: move TokenService to TokenEnpoint namespace

---
+++ ScuffedAuth/Authorization/TokenEndpoint/TokenService.cs
@@ -0,0 +1,18 @@
+﻿using ScuffedAuth.Authorization.ClientCredentials;
+
+namespace ScuffedAuth.Authorization.TokenEndpoint
+{
+    public class TokenService : ITokenService
+    {
+        public TokenResponse GetToken(string authorizationHeader, TokenRequest request)
+        {
+            if (request.GrantType == GrantTypes.client_credentials)
+            {
+                var tokenProvider = new ClientCredentialsAuthorization();
+                return tokenProvider.GetToken(authorizationHeader);
+            }
+
+            return new TokenResponse("Grant type must be defined.");
+        }
+    }
+}
