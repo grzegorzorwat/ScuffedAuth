f6f0485270d2d7aa6f4ce821e5b4a7c3b1f5e711
grzegorzorwat <grzegorz.orwat95@gmail.com>
2020-11-26 22:22:07 +0100

    feat: unidentified authorization

---
+++ ScuffedAuth/Authorization/IAuthorization.cs
@@ -0,0 +1,9 @@
+﻿using ScuffedAuth.Authorization.TokenEndpoint;
+
+namespace ScuffedAuth.Authorization
+{
+    public interface IAuthorization
+    {
+        TokenResponse GetToken(string authorizationHeader);
+    }
+}
---
+++ ScuffedAuth/Authorization/UnidentifiedAuthorization.cs
@@ -0,0 +1,12 @@
+﻿using ScuffedAuth.Authorization.TokenEndpoint;
+
+namespace ScuffedAuth.Authorization
+{
+    public class UnidentifiedAuthorization : IAuthorization
+    {
+        public TokenResponse GetToken(string authorizationHeader)
+        {
+            return new TokenResponse("Grant type must be defined.");
+        }
+    }
+}
