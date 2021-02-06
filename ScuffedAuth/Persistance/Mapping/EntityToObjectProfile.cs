34637b18d6602d0c211d02fea9a4970b08cd4007
grzegorzorwat <grzegorz.orwat95@gmail.com>
2021-02-06 18:57:45 +0100

    fix: TokenEntity to Token mapping

--- ScuffedAuth/Persistance/Mapping/EntityToObjectProfile.cs
+++ ScuffedAuth/Persistance/Mapping/EntityToObjectProfile.cs
@@ -2,6 +2,7 @@
 using Authorization.TokenEndpoint;
 using AutoMapper;
 using ScuffedAuth.Persistance.Entities;
+using System;
 using AuthorizationEndpoint = Authorization.AuthorizationEndpoint;
 using ClientCredentials = Authentication.ClientCredentials;
 
@@ -13,7 +14,9 @@ namespace ScuffedAuth.Persistance.Mapping
         {
             CreateMap<ClientEntity, ClientCredentials.Client>();
             CreateMap<ClientEntity, AuthorizationEndpoint.Client>();
-            CreateMap<TokenEntity, Token>();
+            CreateMap<TokenEntity, Token>()
+                .ForMember(dest => dest.Code, opt => opt.MapFrom(src => src.Value))
+                .ForMember(dest => dest.ExpiresIn, opt => opt.MapFrom(src => TimeSpan.FromSeconds(src.ExpiresIn)));
             CreateMap<AuthorizationCodeEntity, AuthorizationCode>();
         }
     }
